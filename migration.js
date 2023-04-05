a4af4fb855931402587fa480dc622175cd701d1f
Nicolò Zandarin <nikzanda@hotmail.it>
2023-04-05 19:38:07 +0200

    product controller findAll

--- migration.js
+++ migration.js
@@ -1,21 +1,6 @@
-// import mysql from 'mysql2'
-// import migration from 'mysql-migrations'
-// import path from 'path'
-// import { fileURLToPath } from 'url'
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename)
-const mysql = require('mysql2')
 const migration = require('mysql-migrations')
+const db = require('./db.js')
 
-const connection = mysql.createPool({
-  connectionLimit: 10,
-  host: 'localhost',
-  user: 'root',
-  port: 3308,
-  database: 'gym'
-})
-
-migration.init(connection, `${__dirname}/migrations`, () => {
+migration.init(db, `${__dirname}/migrations`, () => {
   console.log('finished running migrations')
 })
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,5 +1,13 @@
+const db = require('../../db.js')
+
 const findAll = (req, res) => {
-  return res.json([])
+  db.query(
+    'SELECT * FROM `products`;',
+    (err, values) => {
+      res.json(values)
+    },
+    (err) => res.sendStatus(500)
+  );
 }
 
 module.exports = {

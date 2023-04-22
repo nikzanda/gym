71247698ce64879f8920f5f06fe1954df5d4cfcd
Nicolò Zandarin <nikzanda@hotmail.it>
2023-04-22 22:48:08 +0200

    dotenv

--- db.js
+++ db.js
@@ -1,11 +1,16 @@
 const mysql = require('mysql2');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: path.join(__dirname, `.env.${process.env.NODE_ENV}`) });
 
 const connection = mysql.createPool({
   connectionLimit: 10,
-  host: 'localhost',
-  user: 'root',
-  port: 3308,
-  database: 'gym',
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  port: process.env.MYSQL_PORT,
 });
 
 module.exports = connection;
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+
 const productRouter = require('./src/routers/product.router');
 
-const app = express();
+dotenv.config({ path: path.join(__dirname, `.env.${process.env.NODE_ENV}`) });
 
+const PORT = process.env.APP_PORT;
+const app = express();
 app.use('/products', productRouter);
 
-const PORT = 3000;
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`⚡️ Server is running at http://localhost:${PORT}`);
